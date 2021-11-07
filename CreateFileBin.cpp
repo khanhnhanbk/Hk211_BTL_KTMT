f550d525905675a350cf84bede86b577fa8ea9ea
Nguyen Nhan <nhanhoa21012002@gmail.com>
2021-11-07 15:02:28 +0700

    add merge funtion

--- CreateFileBin.cpp
+++ CreateFileBin.cpp
@@ -9,13 +9,20 @@ int main()
     string filename = "INT15.BIN";
     ofstream ofs;
     ofs.open(filename, ios::out | ios::binary | ios::trunc);
-    int data[15];
-    for (int i = 0; i < 15; i++)
-    {
-        // randome data value
-        data[i] = rand() % 100;
-    }
+    int data[15] = {1,3,5,2,4,6,100,2,4,6,8,14,20,30,40};
+    // for (int i = 0; i < 15; i++)
+    // {
+    //     // randome data value
+    //     data[i] = rand() % 100;
+    //     cout << data[i] << ' ';
+    // }
     ofs.write((char*)data,60);
     ofs.close();
+    ofstream textFile("data.txt");
+        
+    textFile.is_open();
+    for (int i = 0; i < 15; i++)
+    textFile << data[i] << ' ';
+    textFile.close();
     return 0;
 }
--- Mn_nhom17.asm
+++ Mn_nhom17.asm
@@ -2,7 +2,8 @@
 		.data
 #variable
 input:		.space		60
-filename:	.asciiz		"init15.bin"
+aux:		.space		60
+filename:		.asciiz		"INT15.bin"
 #promt
 prompt_print:	.asciiz		"Du lieu hien tai la: "
 space:		.asciiz		" "
@@ -25,7 +26,7 @@ main:
 	addi	$v0,	$zero,	14	
 	add	$a0,	$s6,	$zero
 	la	$a1,	input
-	addi	$a2,	$zero,	60
+	addi	$a2,	$zero,	600
 	syscall	
 	# close file
 	addi	$v0,	$zero,	16	
@@ -35,9 +36,13 @@ main:
 	jal	print
 #process
 	la	$t1, 	input
+	move	$a0,	$t1
 	la	$a1,	($t1)
-	la	$a2,	60($t1)
-	jal	mergeSort
+	la	$a2,	24($t1)
+	la	$a3,	12($t1)
+	
+	jal	merge
+	jal	print
 #exit
 	addi	$v0,	$zero,	10
 	syscall
@@ -69,17 +74,96 @@ end_for:
 	jr	$ra
 	
 	
-	
 #mergeSort function
-#input
-#$a1: address start
-#$a2: address end
-mergeSort:
-	slt	$t0,	$a1,	$a2
-	beq	$t0,	$zero,	end_mergeSort
-	jal 	print	
-end_mergeSort:
 	jr	$ra
-#input
 #mergeFunction
+merge:
+    #a0 = address input, a1 = address left, a2 = address right,a3 = address mid
+	# store paramenter
+	addi   $sp,$sp,-20
+	sw	$a0, 0($sp)
+	sw	$a1, 4($sp)
+	sw	$a2, 8($sp)
+	sw	$a3, 12($sp)
+	sw	$ra, 20($sp)
+	#t0 = aux
+	la	$t0, aux
+	# while (left < mid && mid < right) so sanh
+	# t4 = mid cux
+	move	$t4, $a3
+while1:
+    # condition
+	slt	$t1, $a1, $t4
+	slt	$t2, $a3, $a2
+	and	$t1, $t1, $t2
+	
+	beqz	$t1, endWhile1 
+    # body while
+        #t1 = *left, t2 = *mid, 
+    	lw	$t1, 0($a1)
+    	lw	$t2, 0($a3)
+        # if (t1 < t2) *aux = t1 else *aux = t2
+        	slt	$t3, $t1, $t2
+        	beqz	$t3, elseIf	
+        # then
+        	sw	$t1, 0($t0)
+        	addi	$a1, $a1, 4
+        	j	endIf
+        # else
+elseIf:
+	sw	$t2, 0($t0)
+	addi	$a3, $a3, 4
+        # endif
+endIf:
+	addi	$t0, $t0, 4
+	j	while1
+    # endWhile
+endWhile1:	
+# 	while (right) chua duyet
+whileRight:
+	slt	$t1, $a3, $a2
+	beqz	$t1, endWhileRight
+    # body while	
+	lw	$t1, 0($a3)
+	sw	$t1, 0($t0)
+	addi	$a3, $a3, 4
+	addi	$t0, $t0, 4
+	j 	whileRight
+    #endwhileRight
+endWhileRight:
+#	while (left) chua duyet het	
+whileLeft:
+	slt	$t1, $a1, $t4
+	beqz	$t1, endWhileLeft
+    # body while	
+	lw	$t1, 0($a1)
+	sw	$t1, 0($t0)
+	addi	$a1, $a1, 4
+	addi	$t0, $t0, 4
+	j 	whileLeft
+    #endwhileRight
+endWhileLeft:
+    # copy from aux to input
+    	lw	$t1, 0($sp)
+	la	$t0, aux
+    # while t1 < right
+whileCopy:
+	slt	$t2, $t1, $a2
+	beqz	$t2, endWhileCopy
+bodyWhileCopy:
+	lw	$t2, 0($t0)
+	sw	$t2, 0($t1)
+	addi	$t0, $t0, 4
+	addi	$t1, $t1, 4
+	j	whileCopy
+endWhileCopy:
+	
+endMerge:
+	lw	$a0, 0($sp)
+	lw	$a1, 4($sp)
+	lw	$a2, 8($sp)
+	lw	$a3, 12($sp)
+	lw	$ra, 20($sp)
+	addi	$sp, $sp, 20
+	jr	$ra
 	
